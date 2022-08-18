95daf28068da5fd47c8b09f5adc7a0f246399bf6
Datagate <datagate_service@example.com>
2022-08-18 14:26:53 +0800

    1.28

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,11 @@ mkdir -p /tmp/github_action
 echo "${ID_RSA_P}" | base64 -d > /tmp/git_action_ssh_id_rsa
 chmod 400 /tmp/git_action_ssh_id_rsa
 
-ssh  -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null -f  -L 0.0.0.0:2222:${TARGET_HOST}:${TARGET_PORT} ${JUMP_USER}@${JUMP_HOST} tail "-f /dev/null"
+export ssh_param=" -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null "
+ssh   ${ssh_param}  -f  -L 0.0.0.0:2222:${TARGET_HOST}:${TARGET_PORT} ${JUMP_USER}@${JUMP_HOST} tail "-f /dev/null"
 
 export uuid=$RANDOM
-export startTime=`ssh  -p 2222 -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null ${TARGET_USER}@127.0.0.1   date +"%m-%d-%y_%H-%M-%S"  `
+export startTime=`ssh  ${ssh_param}  ${TARGET_USER}@127.0.0.1   date +"%m-%d-%y_%H-%M-%S"  `
 export uuid=${startTime}_${uuid}
 echo "uuid is "${uuid}
 
@@ -24,9 +25,9 @@ echo "------main stage------"
 export stage=main
 export STAGE_COMMAND="${COMMAND}"
 /runcommand.sh || true
-echo ">>>>>>main log start>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>main log start<<<<<<<<<<<<<<<<<<<<<<<<<<"
 cat /tmp/github_action/${stage}-github-aciton.log
-echo ">>>>>>main log end>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>main log end<<<<<<<<<<<<<<<<<<<<<<<<<<"
 echo ""
 echo ""
 echo ""
@@ -34,15 +35,17 @@ echo "------post stage------"
 export stage=post
 export STAGE_COMMAND="${POSTCOMMAND}"
 /runcommand.sh || true
-echo ">>>>>>post log start>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>post log start<<<<<<<<<<<<<<<<<<<<<<<<<<"
 cat /tmp/github_action/${stage}-github-aciton.log
-echo ">>>>>>post log end>>>>>>"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>post log end<<<<<<<<<<<<<<<<<<<<<<<<<<"
 
 
 export returnCode=`cat /tmp/github_action/main-github-aciton-code.txt`
 export runlog=`cat /tmp/github_action/main-github-aciton.log`
 
-#ssh  -p 2222 -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null ${TARGET_USER}@127.0.0.1 "rm -rf /tmp/github_action/${uuid}/*" || true
+#ssh  ${ssh_param}  ${TARGET_USER}@127.0.0.1 "rm -rf /tmp/github_action/${uuid}/*" || true
 
 echo "::set-output name=runlog::$runlog"
 echo "::set-output name=returnCode::$returnCode"
+
+exit $returnCode
--- runcommand.sh
+++ runcommand.sh
@@ -10,22 +10,23 @@ chmod +x /tmp/github_action/${stage}_remotescript.sh
 
 sleep 2
 
-ssh  -p 2222 -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null ${TARGET_USER}@127.0.0.1 "mkdir -p /tmp/github_action/${uuid}"
+ssh  -p 2222 ${ssh_param} ${TARGET_USER}@127.0.0.1 "mkdir -p /tmp/github_action/${uuid}"
 
-scp -P 2222 -o 'StrictHostKeyChecking=no' -o 'IdentitiesOnly=yes' -i /tmp/git_action_ssh_id_rsa -F /dev/null /tmp/github_action/${stage}_remotescript.sh ${TARGET_USER}@127.0.0.1:/tmp/github_action/${uuid}/${stage}-github-aciton.sh
+scp -P 2222 ${ssh_param} /tmp/github_action/${stage}_remotescript.sh ${TARGET_USER}@127.0.0.1:/tmp/github_action/${uuid}/${stage}-github-aciton.sh
 
-ssh  -p 2222 -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null ${TARGET_USER}@127.0.0.1 "chmod +x /tmp/github_action/${uuid}/${stage}-github-aciton.sh" || true
+ssh  -p 2222 ${ssh_param} ${TARGET_USER}@127.0.0.1 "chmod +x /tmp/github_action/${uuid}/${stage}-github-aciton.sh" || true
 
 export COMMAND="set -o pipefail; /tmp/github_action/${uuid}/${stage}-github-aciton.sh  2>&1 | tee /tmp/github_action/${uuid}/${stage}-github-aciton.log || echo \$? > /tmp/github_action/${uuid}/${stage}_remotescript_returncode.txt "
 
-echo "##########run in target host stage: ${stage}  ##########"
-ssh  -p 2222 -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null ${TARGET_USER}@127.0.0.1 "${COMMAND}" 
+echo "##########Run in target host stage: ${stage}  start ##########"
+ssh  -p 2222 ${ssh_param} ${TARGET_USER}@127.0.0.1 "${COMMAND}" 
+echo "##########Run in target host stage: ${stage}  end ##########"
 
-export returnCode=`ssh  -p 2222 -o  StrictHostKeyChecking=no -o IdentitiesOnly=yes -i /tmp/git_action_ssh_id_rsa -F /dev/null ${TARGET_USER}@127.0.0.1 "cat /tmp/github_action/${uuid}/${stage}_remotescript_returncode.txt"`
+export returnCode=`ssh  -p 2222 ${ssh_param} ${TARGET_USER}@127.0.0.1 "cat /tmp/github_action/${uuid}/${stage}_remotescript_returncode.txt"`
 
 echo $returnCode > /tmp/github_action/${stage}-github-aciton-code.txt
 
-scp -P 2222 -o 'StrictHostKeyChecking=no' -o 'IdentitiesOnly=yes' -i /tmp/git_action_ssh_id_rsa -F /dev/null ${TARGET_USER}@127.0.0.1:/tmp/github_action/${uuid}/${stage}-github-aciton.log /tmp/github_action/${stage}-github-aciton.log
+scp -P 2222 ${ssh_param} ${TARGET_USER}@127.0.0.1:/tmp/github_action/${uuid}/${stage}-github-aciton.log /tmp/github_action/${stage}-github-aciton.log
 
 
 
